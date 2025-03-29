5e6cb77833ea119ce07d745db5c58850a463d79a
Thais Oliveira <thaisopereira02@gmail.com>
2025-03-29 12:04:38 -0300

    Metodos

--- SIstemaGestaoEscolar/src/models/Professor.java
+++ SIstemaGestaoEscolar/src/models/Professor.java
@@ -1,4 +1,34 @@
 package models;
 
 public class Professor {
+    private String nomeProfessor;
+    private String disciplinaProfessor;
+    private int idadeProfessor;
+
+
+    public Professor(String nomeProfessor, String disciplinaProfessor, int idadeProfessor) {
+        this.nomeProfessor = nomeProfessor;
+        this.disciplinaProfessor = disciplinaProfessor;
+        this.idadeProfessor = idadeProfessor;
+    }
+
+    public int getIdadeProfessor() {
+        return idadeProfessor;
+    }
+
+    public String getNomeProfessor() {
+        return nomeProfessor;
+    }
+
+    public String getDisciplinaProfessor() {
+        return disciplinaProfessor;
+    } 
+
+
+    @Override
+    public String toString(){
+        return "Professor{"+ nomeProfessor + '\'' +
+                "Nome" + idadeProfessor +  '\'' +
+                "Disciplina" + disciplinaProfessor +"}";
+    }
 }
