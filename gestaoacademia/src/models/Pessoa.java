c2a0e09a854eae38098a9c441c0c6e6e013fb815
Thais Oliveira <thaisopereira02@gmail.com>
2025-04-05 14:24:03 -0300

    Criação de classes

---
+++ gestaoacademia/src/models/Pessoa.java
@@ -0,0 +1,21 @@
+package models;
+
+abstract class Pessoa {
+    protected String nomePessoa;
+    protected int idadePessoa;
+
+    public Pessoa(String nome, int idadePessoa) {
+        this.nomePessoa = nome;
+        this.idadePessoa = idadePessoa;
+    }
+
+    public String getNome() {
+        return nomePessoa;
+    }
+
+    public int getIdadePessoa() {
+        return idadePessoa;
+    }
+
+
+}
